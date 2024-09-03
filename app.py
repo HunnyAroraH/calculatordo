a8761c8bf9afe5d12abf7602077ecbb49c38dfe5
HunnyAroraH <parveenarora402@gmail.com>
2024-09-03 19:24:27 +0530

    3 days

--- app.py
+++ app.py
@@ -62,11 +62,8 @@ def fetch_shop_now_link(service_link):
     options.add_argument('--disable-gpu')
     options.add_argument('--remote-debugging-port=9222')
     options.add_argument('--window-size=1920x1080')
-    options.add_argument('--disable-extensions')
-    options.add_argument('--disable-software-rasterizer')
     options.add_argument('--no-first-run')
     options.add_argument('--disable-background-networking')
-    options.add_argument('--disable-default-apps')
     options.binary_location = chrome_binary_path  # Set the Chrome binary path
 
     service = ChromeService(executable_path=chromedriver_path)
